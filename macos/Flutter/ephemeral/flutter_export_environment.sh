a0079512507c61f9579bd51e866b98473dbd3652
App-Dav-3 <appgraphicscycle3@gmail.com>
2025-05-03 20:40:32 +0600

    remove sign background

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\photo\document_scanner_optimize"
+export "FLUTTER_APPLICATION_PATH=D:\Flutter Projects\document_scanner_optimize"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.5"
