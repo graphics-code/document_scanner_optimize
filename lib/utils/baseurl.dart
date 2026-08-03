16a46d4ddbc5649317ff2268001e7eb4ece838cd
app-3 <appgraphicscycle2@gmail.com>
2026-08-03 09:21:11 +0600

    baseurl change to server url

--- lib/utils/baseurl.dart
+++ lib/utils/baseurl.dart
@@ -1,2 +1,2 @@
-//const String baseUrl = "https://clippingworld.cloud";
-const String baseUrl = "http://192.168.1.117:8000"; // local test
+const String baseUrl = "https://clippingworld.cloud";
+//const String baseUrl = "http://192.168.1.117:8000"; // local test
